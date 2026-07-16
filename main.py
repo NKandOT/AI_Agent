8847e0e175391f724ed6dbf69e7275c63200e00a
Luke_Payne <l.a.n.q.payne@gmail.com>
2026-07-16 02:44:13 +0100

    C1L4 finished: Token data printing, and agent_output not None guard installed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 if api_key == None: raise RuntimeError("No API key found")
+user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 client = OpenAI(
 	base_url="https://openrouter.ai/api/v1",
 	api_key=api_key,
@@ -14,8 +15,15 @@ agent_output = client.chat.completions.create(
 	messages=[
 		{
 			"role": "user",
-			"content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+			"content": user_prompt,
 		}
 	]
 )
+if agent_output.usage is None: raise RuntimeError("No usage data returned from API.")
+prompt_tokens = agent_output.usage.prompt_tokens
+response_tokens = agent_output.usage.completion_tokens
+print(f"User prompt: {user_prompt}")
+print(f"Prompt tokens: {prompt_tokens}")
+print(f"Response tokens: {response_tokens}")
+print("Response: ")
 print(agent_output.choices[0].message.content)
