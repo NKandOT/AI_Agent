e38dd2091ac4263fd760d4c697a99d2fb8798835
Luke_Payne <l.a.n.q.payne@gmail.com>
2026-07-21 04:57:51 +0100

    C2L3 completed: get_files_info.py now returns a string of files, their size, and if they are a directory on successful use.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,4 +6,14 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
     valid_tar_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
     if valid_tar_dir == False: return f'Error: Cannot list "{directory}" as it is outside the working directory'
     if os.path.isdir(target_dir) == False: return f'Error: "{directory}" is not a directory'
-    return f'Success: "{directory}" is a directory'
+    list_dir = os.listdir(target_dir)
+    success_output = ""
+    try:
+        for item in list_dir:
+            item_path = os.path.join(target_dir, item)
+            success_output += f"    - {item}: file_size={os.path.getsize(item_path)}, is_dir={os.path.isdir(item_path)} \n"
+    except Exception as e:
+        return f"Error: {e}"
+    return success_output
+    
+    
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,10 +1,13 @@
 from functions.get_files_info import get_files_info
 
 def main():
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
-    print(get_files_info("calculator", "main.py"))
+    lable = "Result for current directory:"
+    cases = [".", "pkg", "/bin", "../"]
+    for directory in cases:
+        print(lable)
+        print(get_files_info("calculator", directory))
+
 
 if __name__ == "__main__":
     main()
+
